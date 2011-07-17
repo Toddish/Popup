43d11eda0fef63d103130e07ef5355eb0ad53c82
Todd Francis <todd@toddish.co.uk>
2011-07-17 23:46:11 +0100

    Updated min version and modal example

--- assets/js/tf-popup.jquery.js
+++ assets/js/tf-popup.jquery.js
@@ -1,7 +1,27 @@
+/*
+ 
+Copyright (C) 2011  Todd Francis
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+
+*/
 ;
 (function($) {
 
-	$.TFPopup = function(element, options) {
+	$.TFPopup = function(element, options) 
+	{
 		 var defaults = {
 			modal						: false,
 			popupWidth					: null,
@@ -22,7 +42,7 @@
 			invalidRowClass				: "invalid_row",
 			errorClass					: "error",
 			closeButton					: {
-				content		: "Close",
+				content		: 'Close',
 				hidden		: false,
 				ID			: "tf_close_button"
 			},
@@ -36,6 +56,8 @@
 		};
 
 		var plugin = this;
+		
+		plugin.version = "0.9.0";
 
 		plugin.settings = {}
 
