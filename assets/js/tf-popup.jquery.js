db44744649c36a705d897b40a53b393350063289
Todd Francis <todd@toddish.co.uk>
2011-07-24 20:25:54 +0100

    recompiled the min version

--- assets/js/tf-popup.jquery.js
+++ assets/js/tf-popup.jquery.js
@@ -64,7 +64,6 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 		var plugin = this;
 		
 		plugin.version = '0.9.0';
-		plugin.$element;
 
 		plugin.settings = {}
 
